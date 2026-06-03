dce43a472e5e7f039e74d75b8f61c80e9e2b69a5
Arpitabirajdar84 <arpitabirajdar123@gmail.com>
2026-06-03 20:33:42 +0530

    FMain

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://smart-parking-backend-pjsc.onrender.com',
+  baseURL: 'https://smart-parking-backend-pjsc.onrender.com', 
 });
 
 export default api;
